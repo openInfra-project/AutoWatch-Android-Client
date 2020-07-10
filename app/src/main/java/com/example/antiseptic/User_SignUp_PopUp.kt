59263e36181f34b2b7ac8987075f4a28dec7e5bd
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2020-07-10 14:20:30 +0900

    초기화면 && 관리자 로그인 화면 && 사용자 로그인 화면 레이아웃 작성

---
+++ app/src/main/java/com/example/antiseptic/User_SignUp_PopUp.kt
@@ -0,0 +1,14 @@
+package com.example.antiseptic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class User_SignUp_PopUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user__sign_up__pop_up)
+
+    }
+}
