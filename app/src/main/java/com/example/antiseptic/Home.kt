e4f88cb7ac19c12ec38ed63a261042cac5b2d143
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2020-07-21 20:58:42 +0900

    Home 도움말 구현

--- app/src/main/java/com/example/antiseptic/Home.kt
+++ app/src/main/java/com/example/antiseptic/Home.kt
@@ -2,15 +2,68 @@ package com.example.antiseptic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
+    private var its: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        code_visible.visibility = View.GONE
         //뒤로가기 버튼
         btn_close_Home.setOnClickListener {
             onBackPressed()
         }
+        //방입장 버튼 클릭시
+        btn_home_join.setOnClickListener {
+            homeAnimation(it = its)
+
+        }
+        val animation  = AnimationUtils.loadAnimation(this,R.anim.home_highlight)
+        image_home_highlight.startAnimation(animation)
+        val animationbackground = AnimationUtils.loadAnimation(this,R.anim.home_highlight_background)
+        Linear_background.startAnimation(animationbackground)
     }
+
+    //버튼클릭시 애니메이션 효과
+    fun homeAnimation(it: Boolean) {
+        if (it) {
+            val animation: TranslateAnimation = TranslateAnimation(
+                0F,
+                0F,
+                0F,
+                60F
+            )
+            animation.duration = 500
+            animation.fillAfter = true
+            btn_home_create.startAnimation(animation)
+            code_visible.startAnimation(animation)
+            code_visible.visibility = View.VISIBLE
+            its = false
+
+        } else {
+            val animation: TranslateAnimation = TranslateAnimation(
+                0F,
+                0F,
+                0F,
+                -40F
+            )
+            animation.duration = 500
+            animation.fillAfter = true
+            btn_home_create.startAnimation(animation)
+            code_visible.startAnimation(animation)
+            code_visible.visibility = View.GONE
+            its = true
+        }
+    }
+
+
 }
+
+
